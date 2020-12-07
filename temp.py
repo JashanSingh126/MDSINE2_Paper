78b671111d441e7fd932d391c077772ba165a904
David Kaplan <dkaplan65@gmail.com>
2020-12-07 14:40:40 -0500

    clean

--- temp.py
+++ temp.py
@@ -26,14 +26,51 @@ md2.LoggingConfig(level=logging.INFO)
 
 # study.taxas.generate_consensus_taxonomies()
 
-chain2 = 'output/negbin2/replicates/mcmc.pkl'
-chain2 = md2.BaseMCMC.load(chain2)
-a1_2 = chain2.graph[STRNAMES.NEGBIN_A1].get_trace_from_disk()
 
-chain1 = 'output/negbin/replicates/mcmc.pkl'
-chain1 = md2.BaseMCMC.load(chain1)
-a1_1 = chain1.graph[STRNAMES.NEGBIN_A1].get_trace_from_disk()
 
-print(np.sum(np.absolute(a1_2-a1_1)))
+#  rows, taxa fmt
+# columns are the clusters
+
+# Get the objects
+mcmc = md2.BaseMCMC.load('output/mdsine2/cv/healthy-cv2/mcmc.pkl')
+clustering = mcmc.graph[STRNAMES.CLUSTERING_OBJ]
+study = mcmc.graph.data.subjects
+
+# Get all the times
+times = study.times(agg='union')
+
+# Get the first time of the perturbation between all of the subjects
+first_time = None
+for subj in study:
+    if first_time is None:
+        first_time = study.perturbations[0].starts[subj.name]
+    else:
+        curr_time = study.perturbations[0].starts[subj.name]
+        if curr_time < first_time:
+            first_time = curr_time
+t_end = np.searchsorted(times, first_time)
+
+# Get mean abundance over subjects for times preperturbation then take mean
+times = times[:t_end]
+M = study.matrix(dtype='abs', agg='mean', times=times)
+abunds = np.mean(M, axis=1)
+
+# Get the clustering
+cidx_assign = md2.generate_cluster_assignments_posthoc(clustering=clustering)
+clustering.from_array(cidx_assign)
+
+# Get the average abundances for each OTU in each cluster
+data = []
+for cluster in clustering:
+    oidxs = list(cluster.members)
+    temp = np.zeros(len(abunds))
+    temp[oidxs] = abunds[oidxs]
+    data.append(temp.reshape(-1,1))
+data = np.hstack(data).shape
+
+# Make the taxonomic heatmap as a dataframe
+fmt = '%(family)s'
+df = md2.condense_matrix_with_taxonomy(M, taxas=study.taxas, fmt=fmt)
+# rows are the family, columns are the clusters
 
 
