58545f76790051e4f497a0782ee8d76ed0212545
dkaplan65 <dkaplan65@gmail.com>
2020-12-15 16:32:30 -0500

    slight debug

--- gibson_dataset/debug_scripts/assign_consensus_taxonomy.sh
+++ gibson_dataset/debug_scripts/assign_consensus_taxonomy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Assign taxonomy for OTUs
-PATH="../../output/debug/processed_data"
-
-echo "Assigning taxonomy to consensus sequences"
-echo "Saving OTUs with assigned taxonomy in ${PATH}"
-
 python ../scripts/assign_taxonomy_for_consensus_seqs.py \
---rdp-table ../files/assign_taxonomy_OTUs/taxonomy_RDP.txt \
---confidence-threshold 50 \
---output-basepath $PATH
+    --rdp-table ../files/assign_taxonomy_OTUs/taxonomy_RDP.txt \
+    --confidence-threshold 50 \
+    --output-basepath ../../output/debug/processed_data
