213f88b2045adc65f93611c96b97ec05904db570
David Kaplan <dkaplan65@gmail.com>
2020-12-15 14:05:54 -0500

    processed data

--- gibson_dataset/assign_consensus_taxonomy.sh
+++ gibson_dataset/assign_consensus_taxonomy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Assign taxonomy for OTUs
-PATH="../processed_data"
-echo "Assigning taxonomy to consensus sequences"
-echo "Saving OTUs with assigned taxonomy in ${PATH}"
 python scripts/assign_taxonomy_for_consensus_seqs.py \
     --rdp-table files/assign_taxonomy_OTUs/taxonomy_RDP.txt \
     --confidence-threshold 50 \
-    --output-basepath $PATH
+    --output-basepath ../processed_data
