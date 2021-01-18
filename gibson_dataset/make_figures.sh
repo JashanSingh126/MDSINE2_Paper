9999892cf8080c9de41585b43ea47d408ed3ec91
Younhun Kim <younhun@mit.edu>
2021-01-18 11:07:32 -0500

    edited make_figures.sh in gibson_dataset

--- gibson_dataset/make_figures.sh
+++ gibson_dataset/make_figures.sh
@@ -7,7 +7,7 @@ python scripts/figure_6.py \
     --uc_cycles_taxa "../output/postprocessing/cycles/unfixed_clustering/uc/paths.csv" \
     --healthy_cycles_clusters "../output/postprocessing/cycles/fixed_clustering/healthy/paths.csv" \
     --uc_cycles_clusters "../output/postprocessing/cycles/fixed_clustering/uc/paths.csv" \
-    --eig_path "../output/postprocessing/eigenvalues.npz" \
+    --eig_path "../output/postprocessing/eigenvalues/eigenvalues.npz" \
     --out_dir "../output" \
     --format "pdf" \
     --dpi 500
