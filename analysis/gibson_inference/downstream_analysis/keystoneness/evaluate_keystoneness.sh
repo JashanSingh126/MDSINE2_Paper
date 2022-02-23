26ba3435a0da033f07136e6efaa9e3c6027008e6
Younhun Kim <younhun@mit.edu>
2022-02-22 20:55:37 -0500

    Consolidated keystoneness to only use CLI interface. Moved Sawal's downstream analyses to subdirectory.

--- analysis/gibson_inference/downstream_analysis/keystoneness/evaluate_keystoneness.sh
+++ analysis/gibson_inference/downstream_analysis/keystoneness/evaluate_keystoneness.sh
@@ -5,10 +5,16 @@ source gibson_inference/settings.sh
 
 echo "Running Keystoneness forward simulations for Healthy dataset."
 
-python gibson_inference/downstream_analysis/keystoneness/evaluate_keystoneness.py \
---input-mcmc-fixed-cluster "${MDSINE_FIXED_CLUSTER_OUT_DIR}/healthy/mcmc.pkl" \
+mdsine2 extract-abundances \
 --study "${MDSINE_FIXED_CLUSTER_OUT_DIR}/healthy/subjset.pkl" \
---out_path $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness/healthy_fwsims.h5 \
+-t 19 \
+-o $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness/healthy-initial_condition.tsv \
+
+mdsine2 evaluate-keystoneness \
+--fixed-cluster-mcmc-path "${MDSINE_FIXED_CLUSTER_OUT_DIR}/healthy/mcmc.pkl" \
+--study "${MDSINE_FIXED_CLUSTER_OUT_DIR}/healthy/subjset.pkl" \
+--initial-conditions $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness/healthy-initial_condition.tsv \
+-o $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness \
 --n-days 64 \
 --simulation-dt 0.01 \
 --limit-of-detection 10000 \
@@ -17,10 +23,16 @@ python gibson_inference/downstream_analysis/keystoneness/evaluate_keystoneness.p
 
 echo "Running Keystoneness forward simulations for Dysbiotic dataset."
 
-python gibson_inference/downstream_analysis/keystoneness/evaluate_keystoneness.py \
---input-mcmc-fixed-cluster "${MDSINE_FIXED_CLUSTER_OUT_DIR}/uc/mcmc.pkl" \
+mdsine2 extract-abundances \
 --study "${MDSINE_FIXED_CLUSTER_OUT_DIR}/uc/subjset.pkl" \
---out-dir $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness/uc_fwsims.h5 \
+-t 19 \
+-o $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness/uc-initial_condition.tsv \
+
+mdsine2 evaluate-keystoneness \
+--fixed-cluster-mcmc-path "${MDSINE_FIXED_CLUSTER_OUT_DIR}/uc/mcmc.pkl" \
+--study "${MDSINE_FIXED_CLUSTER_OUT_DIR}/uc/subjset.pkl" \
+--initial-conditions $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness/uc-initial_condition.tsv \
+-o $DOWNSTREAM_ANALYSIS_OUT_DIR/keystoneness \
 --n-days 64 \
 --simulation-dt 0.01 \
 --limit-of-detection 10000 \
